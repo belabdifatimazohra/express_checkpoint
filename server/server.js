78ada6c61d3331adca2aa2dce81366674f660170
Fatima Zohra <fatima.zohra.belabdi@gmail.com>
2020-12-26 20:25:27 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express"); // Import express from "express"
+const morgan = require('morgan');
+require("dotenv").config(); //dotenv
+
+const PORT = process.env.PORT || 2500; // listen to the port
+const app = express(); // initialise the app
+
+app.use(morgan("tiny"));
+// middleware function in Express. It serves static files
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.use(addActiveTime = (req, res, next) => {
+
+    let requestAt = new Date().getHours();
+    let day = new Date().getDay();
+    if ((requestAt < 9) || (requestAt > 17) && ((day == 0) || (day == 6))) {
+        res.sendFile(__dirname + '/public/Closed.html')
+
+    }
+    else {
+
+        next()
+    }
+})
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+})// listen to the port
